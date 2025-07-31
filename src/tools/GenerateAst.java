cc3926593a94ab5467acc9f4e8810de17c0e6c8c
Boris Chen <boris.chen.czy@gmail.com>
2025-07-31 23:44:29 +0800

    Add Ast generator

---
+++ src/tools/GenerateAst.java
@@ -0,0 +1,90 @@
+package tools;
+
+/*
+
+target
+package com.craftinginterpreters.lox;
+
+abstract class Expr {
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+
+  // Other expressions...
+}
+*/
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1)
+        {
+            System.out.println("Usage: GenerateAst <output_folder>");
+            System.exit(64);
+        }
+
+        String outputFolder = args[0];
+        generateAst(outputFolder, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private static void generateAst(String outputFolder, String baseName, List<String> types) throws IOException {
+        String path = String.format("%s/%s.java", outputFolder, baseName);
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        for (String type: types)
+        {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fields)
+    {
+        writer.printf("    static class %s extends %s {\n", className, baseName);
+        writer.printf("      %s(%s) {\n", className, fields);
+        String[] fieldList = fields.split(",");
+        for (String field: fieldList)
+        {
+            // System.out.println(field);
+            String name = field.trim().split(" ")[1];
+            writer.printf("      this.%s = %s;\n", name, name);
+        }
+        writer.printf("    }\n\n");
+
+        for (String field: fieldList)
+        {
+            writer.printf("    final %s;\n", field);
+        }
+        writer.printf("  }\n\n");
+    }
+}
